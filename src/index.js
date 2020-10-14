bbeae25d8002b66bc45553551d652bdbe016f9d3
Rick_Belson <belsonrj@aol.com>
2020-10-14 14:29:22 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -26,17 +26,6 @@ function candyReducer(state = [], action) {
   }
 }
 
-function countReducer(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENT_COUNT':
-      return state + 1;
-    case 'DECREMENT_COUNT':
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
 
 function render() {
   let container = document.getElementById('container');
